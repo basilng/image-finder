9a6ceaba49bf3fd36fc55536c2c6c2ebd27e9a81
Basil N G <basilng.bng@gmail.com>
2020-10-11 02:09:59 +0530

    Handling Requests with Async Await

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,15 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID pWYG5y--jU3_GQ1WFLKJdVVVedTiR1D4NbBQxodKFtU",
-        },
-      })
-      .then((response) => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID pWYG5y--jU3_GQ1WFLKJdVVVedTiR1D4NbBQxodKFtU",
+      },
+    });
+
+    console.log(response.data.results);
   }
   render() {
     return (
